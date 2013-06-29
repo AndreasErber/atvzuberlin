41efc2c64dad77728fc8e21492c78119ced32481
AndreasErber <post@andreas-erber.net>
2013-06-29 23:55:56 +0200

    Static webpages are handled by this class

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,7 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.i18n.Messages
 import controllers.ext.ProvidesCtx
 import controllers.ext.Security
 
@@ -9,5 +10,17 @@ object Application extends Controller with ProvidesCtx with Security {
 
   def index = Action { implicit request =>
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def about = Action { implicit request =>
+    Ok(views.html.about(Messages("about")))
+  }
+
+  def history = Action { implicit request =>
+    Ok(views.html.history(Messages("history")))
+  }
+
+  def house = Action { implicit request =>
+    Ok(views.html.house(Messages("house")))
   }
 }
