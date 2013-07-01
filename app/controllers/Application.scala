6a615b728d4858fba96a61fdaf6b8d5f0cc0f812
AndreasErber <post@andreas-erber.net>
2013-07-01 10:13:01 +0200

    handling of new static pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -23,4 +23,32 @@ object Application extends Controller with ProvidesCtx with Security {
   def house = Action { implicit request =>
     Ok(views.html.house(Messages("house")))
   }
+
+  def sports = Action { implicit request =>
+    Ok(views.html.sports(Messages("sports")))
+  }
+
+  def sportsdates = Action { implicit request =>
+    Ok(views.html.sportsdates(Messages("sports.dates")))
+  }
+
+  def sportsactives = Action { implicit request =>
+    Ok(views.html.sportsactives(Messages("sports.actives")))
+  }
+
+  def rowing = Action { implicit request =>
+    Ok(views.html.rowing(Messages("sports.rowing")))
+  }
+
+  def handball = Action { implicit request =>
+    Ok(views.html.handball(Messages("sports.handball")))
+  }
+
+  def fistball = Action { implicit request =>
+    Ok(views.html.fistball(Messages("sports.fistball")))
+  }
+
+  def volleyball = Action { implicit request =>
+    Ok(views.html.volleyball(Messages("sports.volleyball")))
+  }
 }
