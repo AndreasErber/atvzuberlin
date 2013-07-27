972884ea2c98a7dcdbdce7cce1ea05b392ea9153
AndreasErber <post@andreas-erber.net>
2013-07-27 22:44:15 +0200

    No actual change

--- app/models/Entity.scala
+++ app/models/Entity.scala
@@ -14,12 +14,12 @@ abstract class Entity(val id: Option[Long] = None, val created: Long = System.cu
 
   /**
    * Redefine the inherited method to limit equality to instances of the same type.
-   * 
+   *
    * @param other The instance to compare this instance with.
    * @return <code>true</code> if the other instance is of the same type, <code>false</code> otherwise.
    */
   override def canEqual(other: Any) = other.isInstanceOf[Entity]
-  
+
   def lastModification(df: DateFormat): String = {
     require(Option(df).isDefined)
     val cal = Calendar.getInstance()
